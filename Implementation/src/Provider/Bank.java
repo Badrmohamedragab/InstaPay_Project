27ee5ac5948f78a21b07334f0eb9590961d3af8d
Badr MOhamed <badrmohamedragab2003@gmail.com>
2023-11-13 14:03:33 +0200

    adding functions for provider and make remove it from wallet and Bank

--- Implementation/src/Provider/Bank.java
+++ Implementation/src/Provider/Bank.java
@@ -5,38 +5,5 @@ import User.*;
 
 public abstract class Bank extends Provider {
 
-	public API getAPI() {
-		// TODO - implement Bank.getAPI
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param api
-	 */
-	public void setAPI(API api) {
-		// TODO - implement Bank.setAPI
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param user
-	 * @param amount
-	 */
-	public void increaseBalance(User user, double amount) {
-		// TODO - implement Bank.increaseBalance
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param user
-	 * @param amount
-	 */
-	public void decreaseBalance(User user, double amount) {
-		// TODO - implement Bank.decreaseBalance
-		throw new UnsupportedOperationException();
-	}
 
 }
--- Implementation/src/Provider/Provider.java
+++ Implementation/src/Provider/Provider.java
@@ -1,44 +1,52 @@
 package Provider;
 
 import APIs.*;
+import DataBase.DBHandle;
 import User.*;
 
 public abstract class Provider {
 
 	private API api;
 
-	public API getAPI() {
-		// TODO - implement Provider.getAPI
-		throw new UnsupportedOperationException();
-	}
 
 	/**
-	 * 
+	 *
 	 * @param api
 	 */
-	public void setAPI(API api) {
-		// TODO - implement Provider.setAPI
-		throw new UnsupportedOperationException();
+
+	public void setApi(API api) {
+		this.api = api;
 	}
+
+	public API getApi() {
+		return api;
+	}
+
 
 	/**
 	 * 
 	 * @param user
 	 * @param amount
 	 */
-	public void increaseBalance(User user, double amount) {
-		// TODO - implement Provider.increaseBalance
-		throw new UnsupportedOperationException();
-	}
+	 public void increaseBalance(User user, double amount)
+	 {
+//		 DBHandle DB = new DBHandle();
+//		 DB.increaseBalance(amount);
+		 user.getAccount().setBalance(user.getAccount().getBalance() + amount);
+	 }
+
 
 	/**
 	 * 
 	 * @param user
 	 * @param amount
 	 */
-	public void decreaseBalance(User user, double amount) {
-		// TODO - implement Provider.decreaseBalance
-		throw new UnsupportedOperationException();
-	}
+	 public void decreaseBalance(User user, double amount)
+	 {
+//		 DBHandle DB = new DBHandle();
+//		 DB.decreaseBalance(amount);
+		 user.getAccount().setBalance(user.getAccount().getBalance() - amount);
+
+	 }
 
 }
