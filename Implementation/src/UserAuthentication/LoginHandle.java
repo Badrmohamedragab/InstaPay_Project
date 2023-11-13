a520393e2bbe598b241493dc9cb292d56003fec8
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-11-13 04:53:02 +0200

    Implement login function

--- Implementation/src/UserAuthentication/LoginHandle.java
+++ Implementation/src/UserAuthentication/LoginHandle.java
@@ -1,15 +1,29 @@
 package UserAuthentication;
 
+/**
+ *<pre>
+ *This class {@code register} its role is login the user
+ *</pre>
+ * <blockquote></blockquote>
+ * @author <strong style="color:'white'"> Mohamed Amir</strong>
+ * @version  <strong style="color:'white'"> 1.0</strong>
+ */
 public class LoginHandle {
 
 	/**
-	 * 
-	 * @param userName
-	 * @param password
+	 *<pre>
+	 *This method {@code login} its role is register the user
+	 *It check if the {@code userName} and {@code Password}
+	 *already exist so will {@code return true}
+	 *else {@code return false}
+	 *</pre>
+	 * @param userName <strong style="color:'white'"> Represent the userName of the user</strong>
+	 * @param password <strong style="color:'white'"> Represent the password of the user</strong>
 	 */
 	public boolean login(String userName, String password) {
-		// TODO - implement LoginHandle.login
-		throw new UnsupportedOperationException();
+		boolean exist = false ;
+		// exist = DBHandle.addUser(user).ifUserExist(userName) ;
+		// exist = exist && DBHandle.addUser(user).ifPasswordCorrect(userName, password) ;
+		return exist ;
 	}
-
 }
