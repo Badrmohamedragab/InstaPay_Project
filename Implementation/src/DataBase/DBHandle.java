cda5afa85c48025b4536018653739c06e72208ad
adel-3 <aboadel1512003@gmail.com>
2023-11-13 15:41:01 +0200

    modify the functions prototybe in th DBHandle class

--- Implementation/src/DataBase/DBHandle.java
+++ Implementation/src/DataBase/DBHandle.java
@@ -1,32 +1,53 @@
 package DataBase;
+
 import User.User;
+
 import java.util.List;
 
 public class DBHandle {
 
-	private static DBConnection dbConnecton;
+    private static DBConnection dbConnecton;
 
-	public static DBConnection getDbConnecton() {
-		return dbConnecton;
-	}
+    public static DBConnection getDbConnecton() {
+        return dbConnecton;
+    }
 
-	/**
-	 * 
-	 * @param db
-	 */
-	public void setDB(DBConnection db) {
-		// TODO - implement DBHandle.setDB
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @param db
+     */
+    public void setDB(DBConnection db) {
+        // TODO - implement DBHandle.setDB
+        throw new UnsupportedOperationException();
+    }
 
-	public DBConnection getDB() {
-		// TODO - implement DBHandle.getDB
-		throw new UnsupportedOperationException();
-	}
-	public List<User> fillUsersList(){
-		return dbConnecton.fillUsersList();
-	}
+    public DBConnection getDB() {
+        // TODO - implement DBHandle.getDB
+        throw new UnsupportedOperationException();
+    }
 
+    public List<User> fillUsersList() {
+        return dbConnecton.fillUsersList();
+    }
+
+    public User ifUserExist(String userName) {
+        return dbConnecton.ifUserExist(userName);
+    }
+
+    public void increaseBalance(User user, double amount) {
+        dbConnecton.increaseBalance(user, amount);
+    }
+
+    public void decreaseBalance(User user, double amount) {
+        dbConnecton.decreaseBalance(user, amount);
+    }
+
+    public boolean isWalletExist(String mobileNumber) {
+        return dbConnecton.isWalletExist(mobileNumber);
+    }
+
+    public boolean ifMobileNumberLocateToAccount(String accountNumber, String mobileNumber) {
+        return dbConnecton.ifMobileNumberLocateToAccount(accountNumber, mobileNumber);
+    }
 
 
 }
